d1f7603bf56e1e88286899e052b2bec0dbb2b761
homepc <punishedtomato@gmail.com>
2023-07-15 22:16:53 -0400

    added basic pygame window for loading a 2 dimensional map on screen from a text file, new assets

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import pygame
+import pygame, os, sys
+from pygame.locals import *
 
-WIDTH, HEIGHT = 900, 500
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.init()
+pygame.display.set_caption('isometric test')
+w, h = 900, 900
+screen = pygame.display.set_mode((w, h), 0, 32)
+display = pygame.Surface((300, 300))
 
+grass_img = pygame.image.load(os.path.join('Assets', 'grass.png')).convert()
+grass_img.set_colorkey((0, 0, 0))  # remove black background in grass.png
 
-def main():
-    run = True
-    while run:  # main game loop
-        for event in pygame.event.get():  # check for different events in pygame
-            if event.type == pygame.QUIT:  # quit event
-                run = False
+f = open(os.path.join('Assets', 'map.txt'))  # open a map file
+map_data = [[int(c) for c in row] for row in f.read().split('\n')]  # loads map data into integers
+f.close()
 
-    pygame.quit()  # quit once the while loop ends
+while True:
+    display.fill((0, 0, 0))
 
+    for y, row in enumerate(map_data):  # iterate through map data
+        for x, tile in enumerate(row):
+            if tile:
+                pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)  # render the tiles
 
-if __name__ == "__main__":  # only run the game if this file is run directly
-    main()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+    screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+    pygame.display.update()
